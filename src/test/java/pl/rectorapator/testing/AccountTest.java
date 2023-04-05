5bd28aba19e45c8a39e13d354895e3bc13c99568
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-04-05 17:02:10 +0200

    05.04.2023

--- src/test/java/pl/rectorapator/testing/AccountTest.java
+++ src/test/java/pl/rectorapator/testing/AccountTest.java
@@ -3,12 +3,35 @@ package pl.rectorapator.testing;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountTest {
 
     @Test
-    void myTest() {
+    void newAccountShouldNotBeActiveAfterCreation() {
+        //given (wstępne założenia testu, tworzymy obiekty, ustawiamy stan początkowy, zaślepiamy zależności)
         Account newAccount = new Account();
+
+        //then (asercje, pod warunkiem, gdy wykonamy jakąś operację to ma się coś stać)
         assertFalse(newAccount.isActive(), "Check if new account is not active");
+    }
+
+     /*BDD:
+            1.Testowanie jednej funkcjonalności, jednej ścieżki wykonania
+            2.W nazwach testów unikamy sformułowań AND i OR
+     */
+
+    @Test
+    void AccountShouldBeActiveAfterActivation() {
+        //given
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+
+        //when (operacje, które chcemy wykonać)
+        newAccount.activate();
+
+        //then
+        assertTrue(newAccount.isActive());
+
     }
 }
