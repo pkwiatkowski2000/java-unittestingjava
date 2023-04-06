444f58bc196ae1dcbf24013a7e7fd2c50d6205dd
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-04-06 15:15:36 +0200

    06.04.2023

---
+++ src/test/java/pl/rectorapator/testing/CartTest.java
@@ -0,0 +1,26 @@
+package pl.rectorapator.testing;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+@DisplayName("Test cases for Cart")
+class CartTest {
+
+    @Test
+    @Disabled
+    @DisplayName("Cart is able to process 1000 orders in 100ms")
+    void simulateLargeOrder() {
+
+        //given
+        Cart cart = new Cart();
+
+        //when
+        //then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
