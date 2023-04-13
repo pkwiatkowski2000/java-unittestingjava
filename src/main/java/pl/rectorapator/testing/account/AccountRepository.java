385f6f832ec80cd3280171e2e116d01f3484b4b8
Paweł <pawel.kwiatkowski2000@gmail.com>
2023-04-13 20:08:00 +0200

    13.04.2023

---
+++ src/main/java/pl/rectorapator/testing/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package pl.rectorapator.testing.account;
+
+import java.util.List;
+
+public interface AccountRepository {
+
+    List<Account> getAllAccounts();
+
+    List<String> getByName(String name);
+}
---
+++ src/main/java/pl/rectorapator/testing/account/AccountService.java
@@ -0,0 +1,23 @@
+package pl.rectorapator.testing.account;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class AccountService {
+
+    private AccountRepository accountRepository;
+
+    AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    List<Account> getAllActiveAccounts() {
+        return accountRepository.getAllAccounts().stream()
+                .filter(Account::isActive)
+                .collect(Collectors.toList());
+    }
+
+    List<String> findByName(String name) {
+        return accountRepository.getByName(name);
+    }
+}
